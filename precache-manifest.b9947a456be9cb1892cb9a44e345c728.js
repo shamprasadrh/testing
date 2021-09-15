e320e38034f17362e4f95dbdb5ffb5e022191766
shamprasadrh <shamprasad.rh@mail.weir>
2021-09-15 17:18:05 +0530

    Updates

---
+++ precache-manifest.b9947a456be9cb1892cb9a44e345c728.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03014015c62d3c178ae24e8cbd6ae4b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9a7f4714c315b14e4e0b",
+    "url": "./static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "3c65fa85e700e955a4d7",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "9a7f4714c315b14e4e0b",
+    "url": "./static/js/2.04c2a71e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.04c2a71e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c65fa85e700e955a4d7",
+    "url": "./static/js/main.e75dd31b.chunk.js"
+  },
+  {
+    "revision": "23512f8096c722fda8cf",
+    "url": "./static/js/runtime-main.1ab298c5.js"
+  }
+]);
